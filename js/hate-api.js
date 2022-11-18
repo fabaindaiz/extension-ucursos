69282fffa4c86a4f16b7a1d4f4d1dfa77569a99f
fabaindaiz <fabiandiaz@ug.uchile.cl>
2022-11-17 23:38:51 -0300

    api funcionando

--- js/hate-api.js
+++ js/hate-api.js
@@ -3,26 +3,39 @@
 const paragraph_limit = 1;
 const text = document.getElementsByClassName('texto');
 
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 /**
-* Function that counts the ammount of paragraphs in a HTML element
+* Function that call the hate-api
 */
-function api_query(message) {
-    $.ajax({
-        url: 'url',
-        type: 'post',
-        data: {str: message},
-        success: function (data) {
-            if (data) {
-                
+async function api_query(text) {
+    fetch('https://ia-api.dev.fadiaz.cl/classify', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "text": text.innerHTML })
+        })
+        .then(response => response.json())
+        .then(data => {
+
+            const options = text.lastElementChild;
+            text.removeChild(options); // Removed
+
+            const long_text = text.innerHTML;
+            const paragraphs = text.innerHTML.split("<br>");
+
+            if (data['inc_label']) {
+                text.innerHTML = '<div><span class="short-text">' + "" + '</span><span class="long-text" style="display: none">' + long_text + '</span><br><button class="show-more-button" data-more="0">Mostrar hate speech</span></div>';
             }
-            else {
-                
-            }
-        },
-        error: function() {
-            
-        }
-    });
+
+            text.append(options);
+
+        })
 }
 
 
@@ -41,7 +54,8 @@ chrome.storage.local.get("settings", function (data) {
     * complete text. Finally we readd the children we had removed.
     */
     for (let i = 0; i < text.length; i++) {
-        const options = text[i].lastElementChild;
+
+        /*const options = text[i].lastElementChild;
         text[i].removeChild(options); // Removed
 
         const long_text = text[i].innerHTML;
@@ -50,6 +64,9 @@ chrome.storage.local.get("settings", function (data) {
         text[i].innerHTML = '<div><span class="short-text">' + "" + '</span><span class="long-text" style="display: none">' + long_text + '</span><br><button class="show-more-button" data-more="0">Mostrar hate speech</span></div>';
         
         text[i].append(options); // Added
+        */
+
+        api_query(text[i])
         
         /*
         const text_length = countLines(text[i]);
@@ -76,29 +93,34 @@ chrome.storage.local.get("settings", function (data) {
      * is clicked and the short text is being displayed it shows the entire text and changes to a button
      * that does the opposite.
      */
-    const buttons = document.querySelectorAll('.show-more-button');
 
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function () {
-            // If text is shown less, then show complete
-            if (this.getAttribute('data-more') === "0") {
-                this.setAttribute('data-more', 1);
-                this.style.display = 'block';
-                this.innerHTML = 'Ocultar hate speech';
+    sleep(2000).then(() => {
 
-                this.previousElementSibling.previousElementSibling.previousElementSibling.style.display = 'none';
-                this.previousElementSibling.previousElementSibling.style.display = 'inline';
-            }
-            // If text is shown complete, then show less
-            else if (this.getAttribute('data-more') === "1") {
-                this.setAttribute('data-more', 0);
-                this.style.display = 'inline';
-                this.innerHTML = 'Mostrar hate speech';
+        const buttons = document.querySelectorAll('.show-more-button');
 
-                this.previousElementSibling.previousElementSibling.previousElementSibling.style.display = 'inline';
-                this.previousElementSibling.previousElementSibling.style.display = 'none';
-            }
-        });
-    }
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', function () {
+                // If text is shown less, then show complete
+                if (this.getAttribute('data-more') === "0") {
+                    this.setAttribute('data-more', 1);
+                    this.style.display = 'block';
+                    this.innerHTML = 'Ocultar hate speech';
+
+                    this.previousElementSibling.previousElementSibling.previousElementSibling.style.display = 'none';
+                    this.previousElementSibling.previousElementSibling.style.display = 'inline';
+                }
+                // If text is shown complete, then show less
+                else if (this.getAttribute('data-more') === "1") {
+                    this.setAttribute('data-more', 0);
+                    this.style.display = 'inline';
+                    this.innerHTML = 'Mostrar hate speech';
+
+                    this.previousElementSibling.previousElementSibling.previousElementSibling.style.display = 'inline';
+                    this.previousElementSibling.previousElementSibling.style.display = 'none';
+                }
+            });
+        }
+
+    });
 
 });
